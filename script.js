eda22ce259d62d76f5cf8651c4bb6ac55ebd52d4
Davidabiale <davinmarcocabal@gmail.com>
2026-02-10 17:22:09 +0800

    update

--- script.js
+++ script.js
@@ -72,7 +72,6 @@ function showMessage(response) {
           document.getElementById('youtube-player-container').style.display = 'block';
           try {
             player.unMute();
-            player.seekTo(50);
             player.playVideo();
           } catch (e) {
             console.log("Autoplay blocked, user interaction needed");
